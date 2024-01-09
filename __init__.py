dffebf5f4ce7783123e47d4144d3d8508353e5ae
SteinerMatthias <86098413+SteinerMatthias@users.noreply.github.com>
2024-01-09 11:00:22 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ import kyber_py.utils
 
 from kyber_py.modules import Module
 from kyber_py.ntt_helper import NTTHelper
-from kyber_py.polynomials import PolynomialRing, Polynomial
+from kyber_py.polynomials import PolynomialRing
 from kyber_py.kyber import Kyber, Kyber512, Kyber768, Kyber1024
