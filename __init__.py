ded909bdade3606958a027c9a327a301e65a2e12
SteinerMatthias <86098413+SteinerMatthias@users.noreply.github.com>
2024-01-09 10:59:58 +0100

    Update __init__.py
    typo fixed

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import kyber_py.utils
 import kyber_py.aes256_ctr_drbg
 import kyber_py.utils
 
-from kyber_py.modules import module
+from kyber_py.modules import Module
 from kyber_py.ntt_helper import NTTHelper
 from kyber_py.polynomials import PolynomialRing, Polynomial
 from kyber_py.kyber import Kyber, Kyber512, Kyber768, Kyber1024
